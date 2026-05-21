152b45a87bbb688cf39488c9be1f3f6e3e63bf46
Yeray Darias Camacho <yeray@ibm.com>
2026-05-21 17:35:01 +0200

    The numbers have maximum 2 decimals

--- src/lib/format.js
+++ src/lib/format.js
@@ -10,10 +10,10 @@ export const fmt = (n, d = 2) =>
  * Format a number as a USD dollar amount with 4 decimal places.
  * @param {number} n
  */
-export const fmtUSD = (n) => "$" + fmt(n, 4);
+export const fmtUSD = (n) => "$" + fmt(n, 2);
 
 /**
  * Format a number as gigabytes with 3 decimal places.
  * @param {number} n
  */
-export const fmtGB = (n) => fmt(n, 3) + " GB";
+export const fmtGB = (n) => fmt(n, 2) + " GB";
